8ae5b8eafe9316f0be59aafee4f1028610fe7ff2
Oliver Masters <contact@olivermasters.co.uk>
2023-07-06 10:14:36 +0100

    Use rest_framework for view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,15 @@
-import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from products.models import Product
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
-    body = request.body
-
+    """DRF API View"""
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-
-    if body:
-        data = json.loads(body)
-
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
-
-    return JsonResponse(data)
+    if model_data:
+        data = model_to_dict(model_data)
+    return Response(data)
