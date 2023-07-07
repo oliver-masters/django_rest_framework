36b0d245c9b346d8258e04f57b30836eb7e1644e
Oliver Masters <contact@olivermasters.co.uk>
2023-07-07 16:43:43 +0100

    combine views single function

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -26,3 +29,31 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     serializer_class = ProductSerializer
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        if pk is not None:
+            queryset = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(queryset).data
+            return Response(data)
+
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get("title")
+            content = serializer.validated_data.get("content") or None
+
+            if content is None:
+                content = title
+
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid"}, status=400)
