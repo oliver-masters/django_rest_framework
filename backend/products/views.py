d7802042481bf87313aa1f2cbd45995583030c4a
Oliver Masters <contact@olivermasters.co.uk>
2023-07-07 15:18:30 +0100

    Add product views with generics

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
+
+        if content is None:
+            content = title
+
+        serializer.save(content=content)
+
+
+product_create_view = ProductCreateAPIView.as_view()
+
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_detail_view = ProductDetailAPIView.as_view()
