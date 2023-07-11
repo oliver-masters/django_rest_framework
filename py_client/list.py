1113b1b605261d8da571aee555f9d4b2052182bb
Oliver Masters <contact@olivermasters.co.uk>
2023-07-11 11:36:19 +0100

    authentication tokens

--- py_client/list.py
+++ py_client/list.py
@@ -1,6 +1,18 @@
 import requests
+import os
 
-endpoint = "http://localhost:8000/api/products/"
 
-get_response = requests.get(endpoint)
-print(get_response.json())
+auth_endpoint = "http://localhost:8000/api/auth/"
+password = os.environ.get("MY_DJANGO_PASSWORD")
+
+auth_response = requests.post(auth_endpoint, json={"username": "masto", "password": password})
+print(auth_response.json())
+
+
+if auth_response.status_code == 200:
+    token = auth_response.json().get("token")
+    headers = {"Authorization": f"Bearer {token}"}
+    endpoint = "http://localhost:8000/api/products/"
+
+    get_response = requests.get(endpoint, headers=headers)
+    print(get_response.json())
