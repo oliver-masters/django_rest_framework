0b82afd05847d0942c7e9524cede42063d6494b1
Oliver Masters <contact@olivermasters.co.uk>
2023-07-11 14:16:54 +0100

    viewsets and routers

---
+++ backend/cfehome/routers.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+
+
+from products.viewsets import ProductGenericViewSet
+
+
+router = DefaultRouter()
+router.register(r'products-abc', ProductGenericViewSet, basename='products')
+
+urlpatterns = router.urls
