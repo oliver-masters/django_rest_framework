86e98f0eaa0d1146cb7b34525aed832c27a99e4f
Oliver Masters <contact@olivermasters.co.uk>
2023-07-12 14:18:45 +0100

    title validation

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,18 +1,20 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Product
+from .validators import validate_title_no_hello, unique_product_title
 
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name="product-detail", lookup_field="pk")
-
+    title = serializers.CharField(validators=[validate_title_no_hello, unique_product_title])
     email = serializers.EmailField(write_only=True)
 
     class Meta:
         model = Product
         fields = [
+            # "user",
             "email",
             "pk",
             "url",
@@ -23,6 +25,12 @@ class ProductSerializer(serializers.ModelSerializer):
             "sale_price",
             "my_discount",
         ]
+
+    # def validate_title(self, value):
+    #     qs = Product.objects.filter(title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError("This title is already in use")
+    #     return value
 
     def create(self, validated_data):
         email = validated_data.pop("email")
