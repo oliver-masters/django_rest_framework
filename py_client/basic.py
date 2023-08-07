71de49e45baf358c3b6d59479d6f0c0886a37624
Oliver Masters <contact@olivermasters.co.uk>
2023-08-07 10:47:26 +0100

    remove basic client view

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,10 +1,18 @@
 import requests
 
+# HTTP Bin is a simple Request and Response service.
+# It's a good resource for checking out how the client requests may react
+# endpoint = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org/anything"
 
-endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/api"
 
-get_response = requests.get(endpoint, params={"abc":123}, json={"query": "Hello world"})
-print(get_response.json())
+
+# A basic get request. This is no longer valid because the project has moved on.
+# Keeping it here for a reminder of what happened in the tutorial.
+
+
+# endpoint = "http://localhost:8000/api"
+#
+# get_response = requests.get(endpoint, params={"abc":123}, json={"query": "Hello world"})
+# print(get_response.json())
 
