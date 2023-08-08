9a952416a5e8c929d908def43e413407482e2547
Oliver Masters <contact@olivermasters.co.uk>
2023-08-08 09:46:38 +0100

    remove demo code
    it's no longer relevant and muddies the message

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -17,18 +17,9 @@ class ProductInLineSerializer(serializers.Serializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     owner = UserPublicSerializer(source="user", read_only=True)
-
-    # Demonstration of nested products
-    # related_products = ProductInLineSerializer(source="user.product_set.all", read_only=True, many=True)
-
-    # Demonstration of foreign key serialization
-    # my_user_data = serializers.SerializerMethodField(read_only=True)
-
-    my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name="product-detail", lookup_field="pk")
     title = serializers.CharField(validators=[validate_title_no_hello, unique_product_title])
-    # email = serializers.EmailField(source="user.email", write_only=True)
 
     class Meta:
         model = Product
@@ -41,44 +32,10 @@ class ProductSerializer(serializers.ModelSerializer):
             "content",
             "price",
             "sale_price",
-            "my_discount",
-            # "my_user_data",
-            # "related_products",
         ]
-
-    # Demonstration of foreign key serialization
-
-    # def get_my_user_data(self, obj):
-    #     return {"username": obj.user.username}
-
-
-    # This is a simple way of doing validation, but validators.py is better
-
-    # def validate_title(self, value):
-    #     qs = Product.objects.filter(title__iexact=value)
-    #     if qs.exists():
-    #         raise serializers.ValidationError("This title is already in use")
-    #     return value
-
-
-    # Example of how one could use an email for a one time event
-
-    # def create(self, validated_data):
-    #     email = validated_data.pop("email")
-    #     print(email)
-    #     obj = super().create(validated_data)
-    #     return obj
-    #
-    # def update(self, instance, validated_data):
-    #     email = validated_data.pop("email")
-    #     print(email)
-    #     return super().update(instance, validated_data)
 
     def get_edit_url(self, obj):
         request = self.context.get("request")
         if request is None:
             return None
         return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
-
-    def get_my_discount(self, obj):
-        return obj.get_discount()
