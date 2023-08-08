c876ad2bc2f8ddcd024b95ebee9dfb51d3a8c00f
Oliver Masters <contact@olivermasters.co.uk>
2023-08-08 16:48:07 +0100

    implement algolia search client

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,10 +1,22 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 from products.models import Product
 from products.serializers import ProductSerializer
 
+from . import client
 
-class SearchListView(generics.ListAPIView):
+
+class SearchListView(generics.GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get('q')
+        if not query:
+            return Response('', status=400)
+        results = client.perform_search(query)
+        return Response(results)
+
+
+class SearchListOldView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
